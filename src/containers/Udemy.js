aef063fa99a720b0f1c349f7647f962cd76e571a
Oleksii Afanasiev <no_blood@Oleksiis-MacBook-Pro.local>
2019-07-24 15:33:23 -0400

    git remote add origin https://github.com/oafanasiev1/routing-assignment.git

---
+++ src/containers/Udemy.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Courses from "../containers/Courses/Courses.js";
+import Users from "../containers/Users/Users.js";
+import { NavLink, Route } from "react-router-dom";
+import "./Udemy.css";
+
+class Udemy extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="Udemy">
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink to="/users">Users</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/courses">Courses</NavLink>
+                </li>
+              </ul>
+            </nav>
+          </header>
+
+            <Route path ="/users" exact component = {Users}/>
+            <Route path ="/courses" exact component = {Courses}/>
+            
+          
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Udemy;
